b503b549f3e2f108a90ba99a12c10e94f9f24a47
socilt54 <socilt54@gmail.com>
2024-02-27 17:43:26 +0900

    Initial commit

---
+++ src/main/java/com/example/first_spring_project/controller/FirstController.java
@@ -0,0 +1,10 @@
+package com.example.first_spring_project.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+    public String niceToMeetYou(){
+        return "";
+    }
+}
