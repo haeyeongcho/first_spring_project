c5543b8cf01da343d37f85ce04302609bf20d8c8
socilt54 <socilt54@gmail.com>
2024-02-28 19:45:28 +0900

    commit

--- README.md
+++
@@ -1,3 +0,0 @@
-스프링 부트, 입문! - 홍팍<br>
-자바 웹 개발, 스프링 부트 입문 강의/강좌<br>
-https://www.youtube.com/playlist?list=PLyebPLlVYXCiYdYaWRKgCqvnCFrLEANXt
--- src/main/java/com/example/first_spring_project/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.first_spring_project.repository;
-
-import com.example.first_spring_project.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-
-}
