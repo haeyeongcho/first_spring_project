4e698f2a77f0e8ffd583b7e06b6ff8da07934083
socilt54 <socilt54@gmail.com>
2024-02-28 19:41:08 +0900

    DTO -> Controller -> Entity -> Repository -> save() -> DB

--- src/main/java/com/example/first_spring_project/controller/FirstController.java
+++ src/main/java/com/example/first_spring_project/controller/FirstController.java
@@ -1,10 +1,21 @@
 package com.example.first_spring_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-    public String niceToMeetYou(){
-        return "";
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "haeyeong");
+        return "greetings"; // templates/greetings.mustache -> 브라우저로 전송
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "cho");
+        return "goodbye";
     }
 }
