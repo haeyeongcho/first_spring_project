dce87707616b6b91fc9b0ce4433bdf0c95d1951a
socilt54 <socilt54@gmail.com>
2024-02-29 15:49:08 +0900

    add lombok, Slf4j

--- src/main/java/com/example/first_spring_project/entity/Article.java
+++ src/main/java/com/example/first_spring_project/entity/Article.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity // DB가 해당 객체를 인식 가능!
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
 
     @Id // 대표값을 지정! like a 주민등록번
@@ -17,19 +23,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
