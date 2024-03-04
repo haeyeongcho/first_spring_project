b40fe26b480cf9b88dfae3ca0af5886dfa509240
socilt54 <socilt54@gmail.com>
2024-03-04 17:41:17 +0900

    edit 기능 추가

--- src/main/java/com/example/first_spring_project/entity/Article.java
+++ src/main/java/com/example/first_spring_project/entity/Article.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity // DB가 해당 객체를 인식 가능!
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor // 디폴트 생성자 추가
 @ToString
+@Getter
 public class Article {
 
     @Id // 대표값을 지정! like a 주민등록번
--- src/main/java/com/example/first_spring_project/repository/ArticleRepository.java
+++ src/main/java/com/example/first_spring_project/repository/ArticleRepository.java
@@ -3,6 +3,9 @@ package com.example.first_spring_project.repository;
 import com.example.first_spring_project.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+import java.util.ArrayList;
 
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
