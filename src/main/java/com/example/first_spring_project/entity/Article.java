3765812db923069d048e8f19c33b0b7894fc584b
socilt54 <socilt54@gmail.com>
2024-03-08 15:16:58 +0900

    기존 CRUD를 REST API로 구현

--- src/main/java/com/example/first_spring_project/entity/Article.java
+++ src/main/java/com/example/first_spring_project/entity/Article.java
@@ -1,10 +1,7 @@
 package com.example.first_spring_project.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity // DB가 해당 객체를 인식 가능! (해당 클래스로 테이블을 만든다!)
 @AllArgsConstructor
@@ -13,7 +10,7 @@ import lombok.ToString;
 @Getter
 public class Article {
 
-    @Id // 대표값을 지정! like a 주민등록번
+    @Id // 대표값을 지정! like a 주민등록번호
     @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션!
     private Long id;
 
@@ -22,4 +19,13 @@ public class Article {
 
     @Column
     private String content;
+
+    public void patch(Article article) {
+        if (article.title != null) {
+            this.title = article.title;
+        }
+        if (article.content != null) {
+            this.content = article.content;
+        }
+    }
 }
